842a6cf8fc7cf60d0f07f431bd3eafd0c25590ce
Jagerziel <fromnytosg@gmail.com>
2023-07-22 22:50:51 -0400

    refactored code to get arrow to work on click

--- input.js
+++ input.js
@@ -1,8 +1,19 @@
 let inputDirection = { x: 0 , y: 0 }
 let lastInputDirection = { x: 0 , y: 0 }
 
-window.addEventListener('keydown', e => {
-    switch(e.key) {
+window.addEventListener('keydown', (e) => updateDirection(e.key))
+document.querySelector("#game-arrow-up").addEventListener('click', () => updateDirection("ArrowUp"))
+document.querySelector("#game-arrow-left").addEventListener('click', () => updateDirection("ArrowLeft"))
+document.querySelector("#game-arrow-right").addEventListener('click', () => updateDirection("ArrowRight"))
+document.querySelector("#game-arrow-down").addEventListener('click', () => updateDirection("ArrowDown"))
+
+export function getInputDirection() {
+    lastInputDirection = inputDirection
+    return inputDirection
+}
+
+function updateDirection ( e ) {
+    switch(e) {
         case 'ArrowUp': 
             if (lastInputDirection.y !== 0 ) break
             inputDirection = { x: 0, y: -1 }
@@ -20,9 +31,4 @@ window.addEventListener('keydown', e => {
             inputDirection = { x: 1, y: 0 }
             break
     }
-})
-
-export function getInputDirection() {
-    lastInputDirection = inputDirection
-    return inputDirection
 }
